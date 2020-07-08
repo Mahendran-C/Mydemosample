b57a536cd3d85cd4183cd886d49ad19b24d93dee
Mahendran-C <mahinew1166@gmail.com>
2020-07-08 22:53:02 +0530

    Changes to be committed:

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 print("This is my first python program")
 print("addition")
-a=input("enter the first number : ")
-b=input("enter the second number : ")
+a=input("enter the first number: ")
+b=input("enter the second number: ")
 c=a+b
 print(c)
